a40d2eea38d6f2622f66897cc2ac270d206feccb
Vaishakh <vaishakh.nargund1999@gmail.com>
2020-07-04 22:18:51 +0530

    updated

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ with open(path.join(this_directory, 'README.md'), encoding='utf-8') as f:
 
 setuptools.setup(
   name = 'pyinsta',         
-  #packages = ['pyinsta'],   
+  packages = ['pyinsta'],   
   version = '0.1',      
   license='GPL 2.0',        
   description = "This package will help you download your favourite celebrity post and also any of your friend's profile picture.",   
@@ -17,7 +17,6 @@ setuptools.setup(
   url = 'https://github.com/vaish1999/Download-insta-posts',  
   download_url = 'https://github.com/vaish1999/Download-insta-posts/archive/v1.0.tar.gz',   
   keywords = ['instagram', 'download', 'profile','picture','post','python'],   
-  packages=setuptools.find_packages(),
   install_requires=[
         'beautifulsoup4',
         'Pillow'
